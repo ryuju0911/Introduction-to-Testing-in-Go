f2513af57dab254f41f7507ac9ae3515bf3624a7
Ryuju0911 <pinebook0911@gmail.com>
2024-05-12 14:03:43 +0900

    [webapp] Add a test for the handlers.

--- webapp/cmd/web/handlers.go
+++ webapp/cmd/web/handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"path"
 )
+
+var pathToTemplates = "./templates/"
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
@@ -14,9 +17,9 @@ type TemplateData struct {
 	Data map[string]any
 }
 
-func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+func (app *application) render(w http.ResponseWriter, _ *http.Request, t string, data *TemplateData) error {
 	// parse the template from disk.
-	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
 	if err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return err
